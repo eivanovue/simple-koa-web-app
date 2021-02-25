966eeb10903bb394260127aa517f403f779fe92f
Emil Ivanov <emil@eivanovue.com>
2021-02-25 09:31:03 +0000

    remove unecessary files

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const bodyParser = require('koa-body');
 const json = require('koa-json');
 const path = require('path');
 const render = require('koa-ejs');
-const fs = require('fs');
 const CrudOperations = require('./dynamodb/crudOperations');
 
 const app = new Koa();
 const router = new KoaRouter();
-const data = require('./data.json');
 
 // JSON Prettier Middleware
 app.use(json());
@@ -31,8 +29,9 @@ router.get('/features', async (ctx) => {
 });
 
 router.get('/all', async (ctx) => {
+  const result = await CrudOperations.getPeople().then((people) => people);
   await ctx.render('all', {
-    people: data,
+    people: result,
   });
 });
 
@@ -43,56 +42,51 @@ router.get('/search', async (ctx) => {
 router.get('/add', async (ctx) => {
   await ctx.render('add', {
     title: 'Add person',
-    people: data,
   });
 });
 
-router.get('/people/search', (ctx) => {
-  console.log(Object.keys(ctx.request.query));
+router.get('/people/search', async (ctx) => {
   const query = Object.keys(ctx.request.query).toString();
   const value = Object.values(ctx.request.query).toString();
-  console.log(query, value);
-  // eslint-disable-next-line max-len
-  const result = data.filter((person) => (person[query].toString().toLowerCase() === value.toLowerCase()));
+  const result = await CrudOperations.getPersonByAtttribute(query, value);
   ctx.body = result;
 });
 
-// filter -> every
-
 router.get('/people/all', async (ctx) => {
-  console.log('trying');
   const result = await CrudOperations.getPeople().then((people) => people);
   ctx.body = result;
 });
 
-router.get('/person', (ctx) => {
-  const result = data.find((person) => person.id === Number(ctx.request.query.id));
+router.get('/person', async (ctx) => {
+  const personId = Number(ctx.request.query.id);
+  const result = await CrudOperations.getPersonById(personId);
   ctx.body = result;
 });
 
 router.get('/person/:id', async (ctx) => {
   const personId = Number(ctx.params.id);
-  const result = await CrudOperations.getPersonById(personId).then((person) => person);
+  const result = await CrudOperations.getPersonById(personId);
   ctx.body = result;
 });
 
 router.post('/create', async (ctx) => {
-  const newId = data[data.length - 1].id + 1;
+  const item = ctx.request.body;
+  const result = await CrudOperations.addPerson(item);
+  ctx.body = result;
+  ctx.redirect('/all');
+});
 
-  data.push({
-    id: newId,
-    first_name: ctx.request.body.first_name,
-    last_name: ctx.request.body.last_name,
-    email: ctx.request.body.email,
-    gender: ctx.request.body.gender,
-  });
+router.put('/update/:id', async (ctx) => {
+  const item = ctx.request.body;
+  const id = Number(ctx.params.id);
+  const result = await CrudOperations.updatePerson(id, item);
+  ctx.body = result;
+});
 
-  // write to JSON file
-  // eslint-disable-next-line no-console
-  await fs.promises.writeFile('./data.json', JSON.stringify(data), 'utf8');
-  // eslint-disable-next-line no-console
-  console.log('Writing to JSON file...');
-  ctx.body = 'Person successfully added!';
+router.delete('/delete/:id', async (ctx) => {
+  const id = Number(ctx.params.id);
+  const result = await CrudOperations.deletePerson(id);
+  ctx.body = result;
 });
 
 app.use(bodyParser());
--- dynamodb/crudOperations.js
+++ dynamodb/crudOperations.js
@@ -1,7 +1,5 @@
 const AWS = require('aws-sdk');
 
-// const fs = require('fs');
-
 AWS.config.update({
   region: 'local',
   endpoint: 'http://localhost:8000',
@@ -11,12 +9,27 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 const table = 'People';
 
 module.exports = {
+  async addPerson(item) {
+    const params = {
+      TableName: table,
+      Item: {
+        id: item.id,
+        first_name: item.first_name,
+        last_name: item.last_name,
+        email: item.email,
+        gender: item.gender,
+      },
+    };
+
+    const result = await docClient.put(params).promise();
+    return result;
+  },
   async getPeople() {
     const params = {
       TableName: table,
     };
-    const result = await docClient.scan(params).promise().then((data) => data.Items);
-    return result;
+    const result = await docClient.scan(params).promise();
+    return result.Items;
   },
   async getPersonById(id) {
     const params = {
@@ -25,7 +38,49 @@ module.exports = {
         id,
       },
     };
-    const result = await docClient.get(params).promise().then((data) => data.Item);
+    const result = await docClient.get(params).promise();
+    return result.Item;
+  },
+  async getPersonByAtttribute(attri, value) {
+    const params = {
+      TableName: table,
+      FilterExpression: '#attri = :value',
+      ExpressionAttributeNames: {
+        '#attri': attri,
+      },
+      ExpressionAttributeValues: {
+        ':value': attri === 'id' ? Number(value) : value,
+      },
+    };
+    const result = await docClient.scan(params).promise();
+    return result.Items;
+  },
+  async updatePerson(id, person) {
+    const params = {
+      TableName: table,
+      Key: {
+        id: Number(id),
+      },
+      UpdateExpression: 'set first_name = :fname, last_name = :lname, gender = :gender, email = :email',
+      ExpressionAttributeValues: {
+        ':fname': person.first_name,
+        ':lname': person.last_name,
+        ':gender': person.gender,
+        ':email': person.email,
+      },
+      ReturnValues: 'UPDATED_NEW',
+    };
+    const result = await docClient.update(params).promise();
+    return result;
+  },
+  async deletePerson(id) {
+    const params = {
+      TableName: table,
+      Key: {
+        id: Number(id),
+      },
+    };
+    const result = await docClient.delete(params).promise();
     return result;
   },
 };
